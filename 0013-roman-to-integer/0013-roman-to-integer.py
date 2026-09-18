95fc0292815b88cc328ae67476756e5eab97c324
Krish Garg <168553165+Krishgarg11@users.noreply.github.com>
2026-09-18 10:17:44 +0530

    Time: 9 ms (55.74%), Space: 12.2 MB (92.18%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def romanToInt(self, s):
+        roman={
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+        integer=0
+        prevint=0
+        for i in range(len(s)-1,-1,-1):
+          currint=roman[s[i]]
+          if currint<prevint:
+            integer-=currint
+          else:
+            integer+=currint
+          prevint=currint
+            
+        return integer
+ 
+        
