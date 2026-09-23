42b272b5c1e3f2700a431cd0cc032dea402ca334
Krish Garg <168553165+Krishgarg11@users.noreply.github.com>
2026-09-23 12:02:35 +0530

    Time: 0 ms (100%), Space: 9.5 MB (78.21%) - LeetHub

--- 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.cpp
+++ 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
     int theMaximumAchievableX(int num, int t) {
-        int x=num+t*2;
-        return x;
+        // int x=num+t*2;
+        // return x;
+        int maxi=0;
+        int x=num;
+        while(t--){
+            x++;
+            num--;
+            if(x<num){
+                num--;
+
+            }
+            else if(x>num){
+            x++;
+            }
+            maxi=max(maxi,x);
+
+        }
+        return maxi;
         
     }
 };
